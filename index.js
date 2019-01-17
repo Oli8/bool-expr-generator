808a599c55943f54018c4380f5d320be6a7341f0
Olivier Crété <olivier.crete@epitech.eu>
2019-01-17 17:59:21 +0100

    Generate method

--- index.js
+++ index.js
@@ -9,4 +9,25 @@ class BoolExpr {
 		}, options);
 	}
 
+	generate() {
+		let expression = this.constructor._arrayRandom(this.options.logicalValues);
+
+		for(let i=0; i<this.options.complexity - 1; i++) {
+			expression += ` ${this._randomOperator()} ${this._randomLogicalValue()}`
+		}
+
+		return expression;
+	}
+
+	static _arrayRandom(array) {
+		return array[Math.floor(Math.random() * array.length)];
+	}
+
+	_randomOperator() {
+		return this.constructor._arrayRandom(this.options.operators);
+	}
+
+	_randomLogicalValue() {
+		return this.constructor._arrayRandom(this.options.logicalValues);
+	}
 }
